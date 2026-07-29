46fec2aa359798a8aaa2c04562824664f88ce8ff
Puneet Meena <puneet04@fusiontecz.com>
2026-07-30 02:02:29 +0530

    Added the Test

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+console.log("URL:", import.meta.env.VITE_SUPABASE_URL);
+console.log("KEY:", import.meta.env.VITE_SUPABASE_ANON_KEY);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
